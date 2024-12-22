1263fbaa855a7ab568aacd1db69ec0eae0371fc8
Maxime Paris <maparis@groupe-atlantic.com>
2024-12-22 20:09:37 +0100

    Fill the board_view_text.c without full option.

--- etape1/board_view_text.c
+++ etape1/board_view_text.c
@@ -4,6 +4,16 @@
 
 #if defined CONFIG_TEXTUI
 
+extern PieceType board[3][3];
+
+char boardText[5][5] = {
+  {' ', '|', ' ', '|', ' '},
+  {'_', '|', '_', '|', '_'},
+  {' ', '|', ' ', '|', ' '},
+  {'_', '|', '_', '|', '_'},
+  {' ', '|', ' ', '|', ' '},
+};
+
 void BoardView_init (void)
 {
 }
@@ -14,27 +24,73 @@ void BoardView_free (void)
 
 void BoardView_displayAll (void)
 {
-  // TODO: à compléter
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+      printf("%c", boardText[i][j]);
+    }
+    printf("\n");
+	}
+  printf("\n\n");
 }
 
 void BoardView_displaySquare (Coordinate x, Coordinate y, PieceType kindOfPiece)
 {
+  char placedPiece;
+  switch (kindOfPiece) {
+    case CROSS:
+      placedPiece = "X";
+      break;
+
+    case CIRCLE:
+      placedPiece = "O";
+      break;
+  }
+  boardText[x][y] = placedPiece;
 	BoardView_displayAll();
 }
 
 void BoardView_displayEndOfGame (GameResult result)
 {
-  // TODO: à compléter
+  BoardView_displayAll();
+  switch (result) {
+  case CIRCLE_WINS:
+    puts("Les cercles ont gagné");
+    break;
+
+  case CROSS_WINS:
+    puts("Les croix ont gagné");
+    break;
+
+  case DRAW:
+    puts("Aucun gagnant");
+    break;
+
+  default:
+    puts("Gagnant invalide")
+    break;
+  }
 }
 
 void BoardView_displayPlayersTurn (PieceType thisPlayer)
 {
-  // TODO: à compléter
+  switch (thisPlayer) {
+    case CIRCLE:
+      puts("C'est au tour des cercles de jouer");
+      break;
+
+    case CROSS:
+      puts("C'est au tour des croix de jouer");
+      break;
+
+    default:
+      puts("Tour de joueur invalide");
+      break;
+  }
 }
 
 void BoardView_sayCannotPutPiece (void)
 {
-  // TODO: à compléter
+  puts("Placement interdit");
 }
 
 #endif // defined CONFIG_TEXTUI
