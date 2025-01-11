a459a638be5f83ccac4d8ab773473579823d8fd3
Maxime Paris <maparis@groupe-atlantic.com>
2025-01-11 23:52:24 +0100

    Stage 2 completed: Implemented player input for moves using scanf

--- etape2/player_manager_scanf.c
+++ etape2/player_manager_scanf.c
@@ -5,15 +5,18 @@
  * @author jilias
  */
 
-#include "board.h"
-#include "board_view.h"
+#include "../etape1/board.h"
+#include "../etape1/board_view.h"
 #include <assert.h>
 #include <stdio.h>
 
 #if defined CONFIG_PLAYER_MANAGER_SCANF
 
+PieceType currentPlayer;
+
 void PlayerManager_init (void)
 {
+	currentPlayer = CROSS;
 }
 
 void PlayerManager_free (void)
@@ -22,7 +25,24 @@ void PlayerManager_free (void)
 
 void PlayerManager_oneTurn (void)
 {
-  // TODO: à compléter
+  int x, y;
+  printf("Joueur %c, entrez les coordonnées de votre coup (ligne colonne) : ", (currentPlayer == CROSS) ? 'X' : 'O');
+  scanf("%d %d", &x, &y);
+  if (Board_putPiece(x, y, currentPlayer) != PIECE_IN_PLACE) {
+    printf("Coup invalide ! Réessayez.\n"); 
+    return;
+  }
+
+  switch (currentPlayer) {
+		case CROSS:
+			currentPlayer = CIRCLE;
+			break;
+		case CIRCLE:
+			currentPlayer = CROSS;
+			break;
+		default:
+      printf("Erreur lors du changement de joueur.");
+	}
 }
 
 #endif // defined CONFIG_PLAYER_MANAGER_SCANF
